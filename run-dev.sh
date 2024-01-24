6e6d30b7d2ffec14a87646738549ec57cab032de
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2024-01-24 16:12:14 +0000

    Tidy up missing files

--- run-dev.sh
+++ run-dev.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 poetry run python /app/manage.py makemigrations
-# poetry run python /app/manage.py squashmigrations app.articles
 poetry run python /app/manage.py migrate --run-syncdb
 poetry run python /app/manage.py createsuperuser --no-input
 poetry run python /app/manage.py runserver 0.0.0.0:8080
